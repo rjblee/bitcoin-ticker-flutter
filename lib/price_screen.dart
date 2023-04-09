4eb35ed9d3f02c2b339835acc8f6db60bcd2ae6f
이문승 <moon@imunseung-ui-MacBookAir.local>
2023-04-10 00:38:42 +0900

    Add: DropDownMenu of currencies

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -6,6 +7,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +45,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text("USD"),
+                  value: "USD",
+                ),
+                DropdownMenuItem(
+                  child: Text("EUR"),
+                  value: "EUR",
+                ),
+                DropdownMenuItem(
+                  child: Text("CAD"),
+                  value: "CAD",
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                });
+              },
+            ),
           ),
         ],
       ),
